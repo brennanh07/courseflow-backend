421f36948e3435d26de0ac2034e1fab7c32c2b11
brennanh07 <brennanh07@vt.edu>
2024-11-03 21:33:40 -0500

    Overall avg gpa display

--- scheduler/schedule_formatter.py
+++ scheduler/schedule_formatter.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
+
 # from logging_config import loggers
 
 # logger = loggers['schedule_formatter']
+
 
 class ScheduleFormatter:
     """
@@ -19,7 +21,7 @@ class ScheduleFormatter:
             date_format (str): The format string to use for displaying times. Defaults to "%I:%M %p".
         """
         self.date_format = date_format
-        
+
     def format_schedule(self, schedule):
         """
         Format a single schedule into a structured dictionary.
@@ -37,11 +39,11 @@ class ScheduleFormatter:
         location_dict = {}
         professor_dict = {}
         gpa_dict = {}
-        
+
         for crn, section_times in schedule.items():
             for section_time in section_times:
                 day_name = section_time.days
-                
+
                 # Handle different day name formats
                 if isinstance(day_name, float):
                     day_name = str(int(day_name))
@@ -49,46 +51,53 @@ class ScheduleFormatter:
                     day_name = day_name.capitalize()
                 else:
                     day_name = "Unknown"
-                
+
                 try:
                     class_info = f"{section_time.crn.course}: {section_time.begin_time.strftime(self.date_format)} - {section_time.end_time.strftime(self.date_format)}"
                 except AttributeError as e:
                     # logger.error(f"Error formatting class info: {e}")
                     class_info = "Error: Unable to format class info"
-                
+
                 day_schedule[day_name].append((section_time.begin_time, class_info))
-                
+
                 # Store CRN information
-                if hasattr(section_time.crn, 'course'):
+                if hasattr(section_time.crn, "course"):
                     if section_time.crn.course not in crn_dict:
                         crn_dict[section_time.crn.course] = section_time.crn.crn
-                        
+
                     location_dict[section_time.crn.course] = section_time.crn.location
                     professor_dict[section_time.crn.course] = section_time.crn.professor
                     gpa_dict[section_time.crn.course] = section_time.crn.avg_gpa
-                    
+
                 else:
                     pass
                     # logger.error(f"section_time.crn does not have 'course' attribute: {section_time.crn}")
 
         # Create an ordered schedule
-        ordered_schedule = {} 
+        ordered_schedule = {}
         for day in ["M", "T", "W", "R", "F", "S", "U"]:
             if day in day_schedule:
-                ordered_schedule[day] = [class_info for _, class_info in sorted(day_schedule[day])]
+                ordered_schedule[day] = [
+                    class_info for _, class_info in sorted(day_schedule[day])
+                ]
             elif "Online" in day_schedule or "Arr" in day_schedule:
-                ordered_schedule["Online/ARR"] = [class_info for _, class_info in sorted(day_schedule.get("Online", []) + day_schedule.get("Arr", []))]
+                ordered_schedule["Online/ARR"] = [
+                    class_info
+                    for _, class_info in sorted(
+                        day_schedule.get("Online", []) + day_schedule.get("Arr", [])
+                    )
+                ]
             else:
                 ordered_schedule[day] = []
-            
+
         return {
             "days": ordered_schedule,
             "crns": crn_dict,
             "locations": location_dict,
             "professors": professor_dict,
-            "gpas": gpa_dict
+            "gpas": gpa_dict,
         }
-        
+
     def print_ranked_schedules(self, top_schedules, top_n=10):
         """
         Format and rank the top schedules.
@@ -104,10 +113,17 @@ class ScheduleFormatter:
             list: A list of dictionaries, each representing a formatted schedule with rank, score, and details.
         """
         formatted_schedules_list = []
-        
+
         for i, (score, schedule) in enumerate(top_schedules[:top_n], 1):
             try:
                 formatted_schedule_data = self.format_schedule(schedule)
+
+                non_null_gpas = [
+                    gpa
+                    for gpa in formatted_schedule_data["gpas"].values()
+                    if gpa is not None
+                ]
+
                 formatted_schedule = {
                     "name": f"Schedule {i}",
                     "score": score,
@@ -116,10 +132,15 @@ class ScheduleFormatter:
                     "locations": formatted_schedule_data["locations"],
                     "professors": formatted_schedule_data["professors"],
                     "gpas": formatted_schedule_data["gpas"],
+                    "schedule_total_avg_gpa": (
+                        round((sum(non_null_gpas) / len(non_null_gpas)), 2)
+                        if non_null_gpas
+                        else None
+                    ),
                 }
                 formatted_schedules_list.append(formatted_schedule)
             except Exception as e:
                 print(f"Error formatting schedule {i}: {e}")
                 # logger.error(f"Error formatting schedule {i}: {e}")
-        
+
         return formatted_schedules_list
